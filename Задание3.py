fe40389d6673672804169366987e4e26b4e500a7
KonstantinVotyakov <164491903+KonstantinVotyakov@users.noreply.github.com>
2024-03-22 15:27:59 +0300

    Create Задание3.py

---
+++ Задание3.py
@@ -0,0 +1,20 @@
+import math
+t = float (input("Введите t: "))
+a = (t+4)**2
+b = t
+c = 3*t-10
+if a == 0:
+   X=-c/b
+  print ('x=', x)
+else:
+   D=b**2-4 * a * c
+   print ("D", D)
+   print("a", a)
+   print ("b", b)
+   print("c", c)
+   if D>=0:
+      x1 - (-b + math.sqrt (0)) / (2*a)
+      x2 - (-b - math.sqrt(®)) / (2*a)
+      print (x1 =', x1, '\nx2 - °,×2)
+else:
+     print ("D<0")
